0e5ca6132a9d7512860779cb6d53f8cd44c93903
anthony4m <amoahanthony41@gmail.com>
2024-11-21 20:08:20 +0000

    #1 exported file name and blknumber

--- kfile/file.go
+++ kfile/file.go
@@ -6,45 +6,45 @@ import (
 )
 
 type BlockId struct {
-	filename string
-	blknum   int
+	Filename string
+	Blknum   int
 }
 
 func NewBlockId(filename string, blknum int) *BlockId {
 	return &BlockId{
-		filename: filename,
-		blknum:   blknum,
+		Filename: filename,
+		Blknum:   blknum,
 	}
 }
 
-func (b *BlockId) Filename() string {
-	return b.filename
+func (b *BlockId) FileName() string {
+	return b.Filename
 }
 
 func (b *BlockId) Number() int {
-	return b.blknum
+	return b.Blknum
 }
 
 func (b *BlockId) Equals(other *BlockId) bool {
 	if other == nil {
 		return false
 	}
-	return b.filename == other.filename && b.blknum == other.blknum
+	return b.Filename == other.Filename && b.Blknum == other.Blknum
 }
 
 func (b *BlockId) String() string {
-	return fmt.Sprintf("[file %s, block %d]", b.filename, b.blknum)
+	return fmt.Sprintf("[file %s, block %d]", b.Filename, b.Blknum)
 }
 
 func (b *BlockId) HashCode() uint32 {
 	h := fnv.New32a()
-	h.Write([]byte(b.filename))
+	h.Write([]byte(b.Filename))
 
 	blknumBytes := []byte{
-		byte(b.blknum >> 24),
-		byte(b.blknum >> 16),
-		byte(b.blknum >> 8),
-		byte(b.blknum),
+		byte(b.Blknum >> 24),
+		byte(b.Blknum >> 16),
+		byte(b.Blknum >> 8),
+		byte(b.Blknum),
 	}
 	h.Write(blknumBytes)
 
