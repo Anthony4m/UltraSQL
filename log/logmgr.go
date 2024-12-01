9c9fbb018b24b2132525720f4f6ed0f38e620753
anthony4m <amoahanthony41@gmail.com>
2024-12-01 08:23:58 +0000

    #13 implemented append flush fuctions

--- log/logmgr.go
+++ log/logmgr.go
@@ -2,8 +2,10 @@ package log
 
 import (
 	"awesomeDB/kfile"
+	"awesomeDB/utils"
 	"fmt"
 	"sync"
+	"unsafe"
 )
 
 type LogMgr struct {
@@ -30,7 +32,7 @@ func newLogMgr(fm *kfile.FileMgr, logFile string) (*LogMgr, error) {
 	} else {
 		b := make([]byte, fm.BlockSize())
 		logMgr.currentBlock = kfile.NewBlockId(logFile, logMgr.logsize-1)
-		newPageBytes := kfile.NewPageFromBytes(b, logFile, logMgr.currentBlock.Blknum)
+		newPageBytes := kfile.NewPageFromBytes(b)
 		pageID := kfile.NewPageId(*logMgr.currentBlock)
 		pageManager.SetPage(pageID, newPageBytes)
 		err := fm.Read(logMgr.currentBlock, pageManager, pageID)
@@ -45,8 +47,44 @@ func newLogMgr(fm *kfile.FileMgr, logFile string) (*LogMgr, error) {
 	return logMgr, nil
 }
 
+func (lm *LogMgr) flushLsn(lsn int) {
+	if lsn >= lm.latestLSN {
+		lm.flush()
+	}
+}
+
+func (lm *LogMgr) Iterator() utils.Iterator[[]byte] {
+	lm.flush()
+	return utils.NewLogIterator(lm.fm, lm.currentBlock)
+}
+
+func (lm *LogMgr) flush() {
+	lm.fm.Write(lm.currentBlock, lm.logPage)
+}
+
 func (lm *LogMgr) appendNewBlock() *kfile.BlockId {
 	newBlock := kfile.NewBlockId(lm.logFile, lm.logsize)
 	lm.logsize++
 	return newBlock
+}
+
+func (lm *LogMgr) append(logrec []byte) int {
+	lm.mu.Lock()
+	defer lm.mu.Unlock()
+	boundary, _ := lm.logPage.GetInt(0)
+	recsize := int32(len(logrec))
+	intBytes := int32(unsafe.Sizeof(int32(0)))
+	bytesNeeded := recsize + intBytes
+
+	if (boundary - bytesNeeded) < intBytes {
+		lm.flush()
+		lm.currentBlock = lm.appendNewBlock()
+		boundary, _ = lm.logPage.GetInt(0)
+	}
+
+	recpos := boundary - bytesNeeded
+	lm.logPage.SetBytes(int(recpos), logrec)
+	lm.logPage.SetInt(0, int(recpos))
+	lm.latestLSN += 1
+	return lm.latestLSN
 }
