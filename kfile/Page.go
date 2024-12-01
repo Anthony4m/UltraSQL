76935a259ad16469facbc87ce0c94d3c9af14f41
anthony4m <amoahanthony41@gmail.com>
2024-12-01 08:27:28 +0000

    #13 refactored page creating to remove the id

--- kfile/Page.go
+++ kfile/Page.go
@@ -7,64 +7,55 @@ import (
 )
 
 type Page struct {
-	data   []byte
-	PageID PageID
+	data []byte
 }
 
-const OUTOFBOUNDS = "offset out of bounds"
+const OUTBOUNDS = "offset out of bounds"
 
 // TODO: Implement the syncronized equivalent in Java
-func NewPage(blockSize int, filename string) *Page {
+func NewPage(blockSize int) *Page {
 
 	return &Page{
 		data: make([]byte, blockSize),
-		PageID: PageID{
-			BlockNumber: blockSize,
-			Filename:    filename,
-		},
 	}
 }
 
-func NewPageFromBytes(b []byte, filename string, blocknumber int) *Page {
+func NewPageFromBytes(b []byte) *Page {
 	dataCopy := make([]byte, len(b))
 	copy(dataCopy, b)
 	return &Page{
 		data: dataCopy,
-		PageID: PageID{
-			BlockNumber: blocknumber,
-			Filename:    filename,
-		},
 	}
 }
 
 func (p *Page) GetInt(offset int) (int32, error) {
 	if offset+4 > len(p.data) {
-		return 0, fmt.Errorf(OUTOFBOUNDS)
+		return 0, fmt.Errorf(OUTBOUNDS)
 	}
 	return int32(binary.BigEndian.Uint32(p.data[offset:])), nil
 }
 
 func (p *Page) SetInt(offset int, val int) error {
 	if offset+4 > len(p.data) {
-		return fmt.Errorf(OUTOFBOUNDS)
+		return fmt.Errorf(OUTBOUNDS)
 	}
 	binary.BigEndian.PutUint32(p.data[offset:], uint32(val))
 	return nil
 }
 
-func (p *Page) GetBytes(offset int, length int) ([]byte, error) {
-	if offset+length > len(p.data) {
-		return nil, fmt.Errorf(OUTOFBOUNDS)
+func (p *Page) GetBytes(offset int) ([]byte, error) {
+	if offset > len(p.data) {
+		return nil, fmt.Errorf(OUTBOUNDS)
 	}
-	dataCopy := make([]byte, length)
-	copy(dataCopy, p.data[offset:offset+length])
+	dataCopy := make([]byte, 0)
+	copy(dataCopy, p.data[offset:offset])
 	return dataCopy, nil
 }
 
 func (p *Page) SetBytes(offset int, val []byte) error {
 	length := len(val)
 	if offset+length > len(p.data) {
-		return fmt.Errorf(OUTOFBOUNDS)
+		return fmt.Errorf(OUTBOUNDS)
 	}
 	copy(p.data[offset:], val)
 	return nil
@@ -72,7 +63,7 @@ func (p *Page) SetBytes(offset int, val []byte) error {
 
 func (p *Page) GetString(offset int, length int) (string, error) {
 	if offset+length > len(p.data) {
-		return "", fmt.Errorf(OUTOFBOUNDS)
+		return "", fmt.Errorf(OUTBOUNDS)
 	}
 
 	str := string(trimZero(p.data[offset : offset+length]))
@@ -85,7 +76,7 @@ func (p *Page) SetString(offset int, val string) error {
 	copy(strBytes, val)
 
 	if offset+length > len(p.data) {
-		return fmt.Errorf(OUTOFBOUNDS)
+		return fmt.Errorf(OUTBOUNDS)
 	}
 	copy(p.data[offset:], strBytes)
 	return nil
@@ -93,7 +84,7 @@ func (p *Page) SetString(offset int, val string) error {
 
 func (p *Page) SetBool(offset int, val bool) error {
 	if offset+1 > len(p.data) {
-		return fmt.Errorf(OUTOFBOUNDS)
+		return fmt.Errorf(OUTBOUNDS)
 	}
 	if val {
 		p.data[offset] = 1
@@ -105,7 +96,7 @@ func (p *Page) SetBool(offset int, val bool) error {
 
 func (p *Page) GetBool(offset int) (bool, error) {
 	if offset+1 > len(p.data) {
-		return false, fmt.Errorf(OUTOFBOUNDS)
+		return false, fmt.Errorf(OUTBOUNDS)
 	}
 	if p.data[offset] == 1 {
 		return true, nil
@@ -115,7 +106,7 @@ func (p *Page) GetBool(offset int) (bool, error) {
 
 func (p *Page) SetDate(offset int, val time.Time) error {
 	if offset+8 > len(p.data) {
-		return fmt.Errorf("offset out of bounds")
+		return fmt.Errorf(OUTBOUNDS)
 	}
 	convertedVal := uint64(val.Unix())
 	binary.BigEndian.PutUint64(p.data[offset:], convertedVal)
@@ -124,7 +115,7 @@ func (p *Page) SetDate(offset int, val time.Time) error {
 
 func (p *Page) GetDate(offset int) (time.Time, error) {
 	if offset+8 > len(p.data) {
-		return time.Unix(0, 0), fmt.Errorf("offset out of bounds")
+		return time.Unix(0, 0), fmt.Errorf(OUTBOUNDS)
 	}
 	timestamp := binary.BigEndian.Uint64(p.data[offset:])
 	return time.Unix(int64(timestamp), 0), nil
