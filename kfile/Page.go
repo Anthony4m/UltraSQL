b74662c39fb5feda93b3159505b754d0d53f53dd
anthony4m <amoahanthony41@gmail.com>
2024-12-06 18:45:59 +0000

    #12 removed page manager

--- kfile/Page.go
+++ kfile/Page.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Page struct {
-	Data   []byte
+	data   []byte
 	pageId uint64
 	mu     sync.RWMutex
 }
@@ -19,17 +19,16 @@ const (
 
 const pageIdOffset = 0
 
-// TODO: Implement the syncronized equivalent in Java
 func NewPage(blockSize int) *Page {
 	page := &Page{
-		Data: make([]byte, blockSize),
+		data: make([]byte, blockSize),
 	}
 	return page
 }
 
 func NewPageFromBytes(b []byte) *Page {
 	page := &Page{
-		Data: b,
+		data: b,
 	}
 	return page
 }
@@ -37,19 +36,19 @@ func NewPageFromBytes(b []byte) *Page {
 func (p *Page) GetInt(offset int) (int, error) {
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	if offset > len(p.Data) {
+	if offset > len(p.data) {
 		return 0, fmt.Errorf("%s: getting int", ErrOutOfBounds)
 	}
-	return int(binary.BigEndian.Uint32(p.Data[offset:])), nil
+	return int(binary.BigEndian.Uint32(p.data[offset:])), nil
 }
 
 func (p *Page) SetInt(offset int, val int) error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	if offset+4 > len(p.Data) {
+	if offset+4 > len(p.data) {
 		return fmt.Errorf("%s: setting int", ErrOutOfBounds)
 	}
-	binary.BigEndian.PutUint32(p.Data[offset:], uint32(val))
+	binary.BigEndian.PutUint32(p.data[offset:], uint32(val))
 	return nil
 }
 
@@ -57,19 +56,19 @@ func (p *Page) GetBytes(offset int) ([]byte, error) {
 	p.mu.Lock()
 	defer p.mu.Unlock()
 
-	if offset > len(p.Data) {
+	if offset > len(p.data) {
 		return nil, fmt.Errorf("%s: getting bytes", ErrOutOfBounds)
 	}
 
 	// Find the end of the segment (delimiter)
 	end := offset
-	for end < len(p.Data) && p.Data[end] != 0 {
+	for end < len(p.data) && p.data[end] != 0 {
 		end++
 	}
 
 	// Copy data between offset and end
 	dataCopy := make([]byte, end-offset)
-	copy(dataCopy, p.Data[offset:end])
+	copy(dataCopy, p.data[offset:end])
 
 	return dataCopy, nil
 }
@@ -81,27 +80,27 @@ func (p *Page) SetBytes(offset int, val []byte) error {
 	length := len(val)
 	// Check if there's enough space for the data and the delimiter
 	if length != 0 {
-		if offset+length+1 > len(p.Data) { // +1 for the delimiter
+		if offset+length+1 > len(p.data) { // +1 for the delimiter
 			return fmt.Errorf("%s: setting bytes", ErrOutOfBounds)
 		}
 
 		// Clear the buffer in the target range
 		for i := 0; i < length+1; i++ { // +1 to clear the delimiter space
-			p.Data[offset+i] = 0
+			p.data[offset+i] = 0
 		}
 
 		// Copy the new value
-		copy(p.Data[offset:], val)
+		copy(p.data[offset:], val)
 
 		// Set the delimiter
-		p.Data[offset+length] = 0 // Null byte as a delimiter
+		p.data[offset+length] = 0 // Null byte as a delimiter
 	}
 
 	return nil
 }
 
 func (p *Page) GetString(offset int) (string, error) {
-	if offset > len(p.Data) {
+	if offset > len(p.data) {
 		return "", fmt.Errorf("%s: getting string", ErrOutOfBounds)
 	}
 
@@ -124,13 +123,13 @@ func (p *Page) SetString(offset int, val string) error {
 func (p *Page) SetBool(offset int, val bool) error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	if offset+1 > len(p.Data) {
+	if offset+1 > len(p.data) {
 		return fmt.Errorf("%s: setting bool", ErrOutOfBounds)
 	}
 	if val {
-		p.Data[offset] = 1
+		p.data[offset] = 1
 	} else {
-		p.Data[offset] = 0
+		p.data[offset] = 0
 	}
 	return nil
 }
@@ -138,10 +137,10 @@ func (p *Page) SetBool(offset int, val bool) error {
 func (p *Page) GetBool(offset int) (bool, error) {
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	if offset+1 > len(p.Data) {
+	if offset+1 > len(p.data) {
 		return false, fmt.Errorf("%s: getting bool", ErrOutOfBounds)
 	}
-	if p.Data[offset] == 1 {
+	if p.data[offset] == 1 {
 		return true, nil
 	}
 	return false, nil
@@ -150,26 +149,26 @@ func (p *Page) GetBool(offset int) (bool, error) {
 func (p *Page) SetDate(offset int, val time.Time) error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	if offset+8 > len(p.Data) {
+	if offset+8 > len(p.data) {
 		return fmt.Errorf("%s: setting date", ErrOutOfBounds)
 	}
 	convertedVal := uint64(val.Unix())
-	binary.BigEndian.PutUint64(p.Data[offset:], convertedVal)
+	binary.BigEndian.PutUint64(p.data[offset:], convertedVal)
 	return nil
 }
 
 func (p *Page) GetDate(offset int) (time.Time, error) {
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	if offset+8 > len(p.Data) {
+	if offset+8 > len(p.data) {
 		return time.Unix(0, 0), fmt.Errorf("%s: getting date", ErrOutOfBounds)
 	}
-	timestamp := binary.BigEndian.Uint64(p.Data[offset:])
+	timestamp := binary.BigEndian.Uint64(p.data[offset:])
 	return time.Unix(int64(timestamp), 0), nil
 }
 
 func (p *Page) Contents() []byte {
-	return p.Data
+	return p.data
 }
 
 func trimZero(s []byte) []byte {
