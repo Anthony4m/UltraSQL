2738118cf3dd8acaa7de1adcbc0140c2ab0b90ad
anthony4m <amoahanthony41@gmail.com>
2024-12-06 18:43:37 +0000

    #12 refactored iterator

--- utils/Iterator.go
+++ utils/Iterator.go
@@ -2,20 +2,5 @@ package utils
 
 type Iterator[T any] interface {
 	HasNext() bool
-	Next() T
-}
-
-type SliceIterator[T any] struct {
-	slice []T
-	index int
-}
-
-func (it *SliceIterator[T]) HasNext() bool {
-	return it.index < len(it.slice)
-}
-
-func (it *SliceIterator[T]) Next() T {
-	value := it.slice[it.index]
-	it.index++
-	return value
+	Next() ([]byte, error)
 }
