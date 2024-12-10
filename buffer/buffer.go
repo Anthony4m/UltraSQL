6e534708e28060dab44f90e9271c4233873c1f9c
anthony4m <amoahanthony41@gmail.com>
2024-12-10 21:26:53 +0000

    #2 added buffer and buffer manager

---
+++ buffer/buffer.go
@@ -0,0 +1,87 @@
+package buffer
+
+import (
+	"awesomeDB/kfile"
+	"awesomeDB/log"
+	"errors"
+)
+
+type Buffer struct {
+	fm       *kfile.FileMgr
+	lm       *log.LogMgr
+	contents *kfile.Page
+	blk      *kfile.BlockId
+	pins     int
+	txnum    int
+	lsn      int
+}
+
+func NewBuffer(fm *kfile.FileMgr, lm *log.LogMgr) *Buffer {
+	return &Buffer{
+		fm:       fm,
+		lm:       lm,
+		contents: kfile.NewPage(fm.BlockSize()),
+		blk:      nil,
+		pins:     0,
+		txnum:    -1,
+		lsn:      -1,
+	}
+}
+
+func (b *Buffer) GetContents() *kfile.Page {
+	return b.contents
+}
+
+func (b *Buffer) Block() *kfile.BlockId {
+	return b.blk
+}
+
+func (b *Buffer) MarkModified(txtnum int, lsn int) {
+	b.txnum = txtnum
+	if lsn > 0 {
+		b.lsn = lsn
+	}
+}
+
+func (b *Buffer) IsPinned() bool {
+	return b.pins > 0
+}
+
+func (b *Buffer) modifyingTx() int {
+	return b.txnum
+}
+
+func (b *Buffer) assignToBlock(block *kfile.BlockId) error {
+	if err := b.flush(); err != nil {
+		return err
+	}
+	b.blk = block
+	if err := b.fm.Read(b.blk, b.contents); err != nil {
+		return err
+	}
+	b.pins = 0
+	return nil
+}
+
+func (b *Buffer) flush() error {
+	if b.txnum > 0 && b.blk != nil {
+		b.lm.FlushLsn(b.lsn)
+		if err := b.fm.Write(b.blk, b.contents); err != nil {
+			return err
+		}
+		b.txnum = -1
+	}
+	return nil
+}
+
+func (b *Buffer) pin() {
+	b.pins++
+}
+
+func (b *Buffer) unpin() error {
+	if b.pins <= 0 {
+		return errors.New("unpin operation failed: buffer is not pinned")
+	}
+	b.pins--
+	return nil
+}
---
+++ buffer/bufferMgr.go
@@ -0,0 +1,126 @@
+package buffer
+
+import (
+	"awesomeDB/kfile"
+	"awesomeDB/log"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+)
+
+type BufferMgr struct {
+	bufferpool   []*Buffer
+	numAvailable int
+	mu           sync.RWMutex
+	cond         *sync.Cond
+}
+
+const MAX_TIME = 1000 * time.Millisecond
+
+func NewBufferMgr(fm *kfile.FileMgr, lm *log.LogMgr, numbuffs int) *BufferMgr {
+	bm := &BufferMgr{
+		bufferpool:   make([]*Buffer, numbuffs),
+		numAvailable: numbuffs,
+	}
+	bm.cond = sync.NewCond(&bm.mu)
+
+	for i := 0; i < numbuffs; i++ {
+		bm.bufferpool[i] = NewBuffer(fm, lm)
+	}
+
+	return bm
+}
+
+func (bM *BufferMgr) available() int {
+	bM.mu.RLock()
+	defer bM.mu.RUnlock()
+	return bM.numAvailable
+}
+
+func (bM *BufferMgr) FlushAll(txtnum int) {
+	bM.mu.RLock()
+	defer bM.mu.RUnlock()
+	for _, buff := range bM.bufferpool {
+		if buff.modifyingTx() == txtnum {
+			buff.flush()
+		}
+	}
+}
+
+func (bM *BufferMgr) unpin(buff *Buffer) {
+	bM.mu.Lock()
+	defer bM.mu.Unlock()
+
+	err := buff.unpin()
+	if err != nil {
+		panic(err)
+	}
+	if !buff.IsPinned() {
+		bM.numAvailable++
+		bM.cond.Broadcast()
+	}
+}
+
+func (bM *BufferMgr) pin(blk *kfile.BlockId) (*Buffer, error) {
+	bM.mu.Lock()
+	defer bM.mu.Unlock()
+
+	starttime := time.Now()
+	buff := bM.tryToPin(blk)
+
+	for buff == nil && !bM.waitingTooLong(starttime) {
+		bM.cond.Wait()
+		buff = bM.tryToPin(blk)
+	}
+
+	if buff == nil {
+		return nil, fmt.Errorf("BufferAbortException: Could not pin the buffer")
+	}
+
+	return buff, nil
+}
+
+func (bM *BufferMgr) waitingTooLong(startTime time.Time) bool {
+	return time.Since(startTime) > MAX_TIME
+}
+
+func (bM *BufferMgr) tryToPin(blk *kfile.BlockId) *Buffer {
+	buff := bM.findExistingBuffer(blk)
+	if buff == nil {
+		buff = bM.chooseUnpinnedBuffer()
+		if buff == nil {
+			return nil
+		}
+		err := buff.assignToBlock(blk)
+		if err != nil {
+			if !strings.Contains(err.Error(), "EOF") {
+				// Panic for errors that do not involve "EOF"
+				panic(err)
+			}
+		}
+	}
+	if !buff.IsPinned() {
+		bM.numAvailable--
+	}
+	buff.pin()
+	return buff
+}
+
+func (bM *BufferMgr) findExistingBuffer(blk *kfile.BlockId) *Buffer {
+	for i := range bM.bufferpool {
+		if bM.bufferpool[i].Block() != nil && bM.bufferpool[i].Block().Equals(blk) {
+			return bM.bufferpool[i]
+		}
+	}
+	return nil
+}
+
+func (bM *BufferMgr) chooseUnpinnedBuffer() *Buffer {
+	for _, buff := range bM.bufferpool {
+		if !buff.IsPinned() {
+			return buff
+		}
+	}
+	return nil
+}
