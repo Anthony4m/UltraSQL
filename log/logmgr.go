247d7b72e7d5e31d68b4884ec4533e0f4c4c38e6
anthony4m <amoahanthony41@gmail.com>
2025-01-01 07:07:14 +0000

    removed I/O operations to buffer, pined a page instead

--- log/logmgr.go
+++ log/logmgr.go
@@ -3,6 +3,7 @@ package log
 import (
 	"fmt"
 	"sync"
+	"ultraSQL/buffer"
 	"ultraSQL/kfile"
 	"ultraSQL/utils"
 	"unsafe"
@@ -20,21 +21,23 @@ func (e *Error) Error() string {
 type LogMgr struct {
 	fm             *kfile.FileMgr
 	mu             sync.RWMutex
+	bm             *buffer.BufferMgr
+	logBuffer      *buffer.Buffer
 	logFile        string
 	currentBlock   *kfile.BlockId
-	logPage        *kfile.Page
 	latestLSN      int
 	latestSavedLSN int
 	logsize        int
 }
 
-func NewLogMgr(fm *kfile.FileMgr, logFile string) (*LogMgr, error) {
+func NewLogMgr(fm *kfile.FileMgr, bm *buffer.BufferMgr, logFile string) (*LogMgr, error) {
 	if fm == nil {
 		return nil, &Error{Op: "new", Err: fmt.Errorf("file manager cannot be nil")}
 	}
 
 	logMgr := &LogMgr{
 		fm:      fm,
+		bm:      bm,
 		logFile: logFile,
 	}
 
@@ -42,29 +45,31 @@ func NewLogMgr(fm *kfile.FileMgr, logFile string) (*LogMgr, error) {
 	if logMgr.logsize, err = fm.Length(logFile); err != nil {
 		return nil, &Error{Op: "new", Err: fmt.Errorf("failed to get log file length: %v", err)}
 	}
-
 	b := make([]byte, fm.BlockSize())
-	logMgr.logPage = kfile.NewPageFromBytes(b)
-
+	logPage := kfile.NewPageFromBytes(b)
 	if logMgr.logsize == 0 {
-		if logMgr.currentBlock = logMgr.appendNewBlock(); logMgr.currentBlock == nil {
+		if logMgr.currentBlock, err = logMgr.appendNewBlock(); logMgr.currentBlock == nil {
 			return nil, &Error{Op: "new", Err: fmt.Errorf("failed to append initial block")}
 		}
+		logMgr.bm.Insert(logMgr.currentBlock)
 	} else {
 		logMgr.currentBlock = kfile.NewBlockId(logFile, logMgr.logsize-1)
-		if err := fm.Read(logMgr.currentBlock, logMgr.logPage); err != nil {
-			return nil, &Error{Op: "new", Err: fmt.Errorf("failed to read current block: %v", err)}
-		}
+	}
+	buff, err := bm.Pin(logMgr.currentBlock)
+	buff.SetContents(logPage)
+	if err != nil {
+		return nil, &Error{Op: "Pin", Err: fmt.Errorf("failed to append initial block")}
 	}
 
+	logMgr.logBuffer = buff
+	if err := logMgr.logBuffer.GetContents().SetInt(0, logMgr.fm.BlockSize()); err != nil {
+		return nil, &Error{Op: "Pin", Err: fmt.Errorf("failed to append initial block")}
+	}
+
+	if err := logMgr.logBuffer.Flush(); err != nil {
+		return nil, &Error{Op: "Pin", Err: fmt.Errorf("failed to append initial block")}
+	}
 	return logMgr, nil
-}
-
-func (lm *LogMgr) FlushLSN(lsn int) error {
-	if lsn >= lm.latestLSN {
-		return lm.Flush()
-	}
-	return nil
 }
 
 func (lm *LogMgr) FlushAsync() <-chan error {
@@ -80,33 +85,30 @@ func (lm *LogMgr) Iterator() (utils.Iterator[[]byte], error) {
 	if err := lm.Flush(); err != nil {
 		return nil, &Error{Op: "iterator", Err: err}
 	}
-	return utils.NewLogIterator(lm.fm, lm.currentBlock), nil
+	return utils.NewLogIterator(lm.fm, lm.bm, lm.currentBlock), nil
 }
 
 func (lm *LogMgr) Flush() error {
-	if err := lm.fm.Write(lm.currentBlock, lm.logPage); err != nil {
-		return &Error{Op: "flush", Err: fmt.Errorf("failed to write block %s: %v",
-			lm.currentBlock.GetFileName(), err)}
+
+	// Flush the log buffer to disk
+	if err := lm.logBuffer.LogFlush(lm.currentBlock); err != nil {
+		return err
+	}
+	if lm.logBuffer != nil {
+		lm.bm.UnPin(lm.logBuffer)
 	}
 	lm.latestSavedLSN = lm.latestLSN
 	return nil
 }
 
-func (lm *LogMgr) appendNewBlock() *kfile.BlockId {
-	newBlock, err := lm.fm.Append(lm.logFile)
+func (lm *LogMgr) appendNewBlock() (*kfile.BlockId, error) {
+	blkNum, err := lm.fm.LengthLocked(lm.logFile)
 	if err != nil {
-		return nil
+		return nil, &Error{Op: "appendNewBlock", Err: err}
 	}
 
-	if err := lm.logPage.SetInt(0, lm.fm.BlockSize()); err != nil {
-		return nil
-	}
-
-	if err := lm.fm.Write(newBlock, lm.logPage); err != nil {
-		return nil
-	}
-
-	return newBlock
+	blk := kfile.NewBlockId(lm.logFile, blkNum)
+	return blk, nil
 }
 
 func (lm *LogMgr) Append(logrec []byte) (int, error) {
@@ -117,7 +119,9 @@ func (lm *LogMgr) Append(logrec []byte) (int, error) {
 	lm.mu.Lock()
 	defer lm.mu.Unlock()
 
-	boundary, err := lm.logPage.GetInt(0)
+	logPage := lm.bm.Get(lm.currentBlock).GetContents()
+
+	boundary, err := logPage.GetInt(0)
 	if err != nil {
 		return 0, &Error{Op: "append", Err: fmt.Errorf("failed to get boundary: %v", err)}
 	}
@@ -131,23 +135,31 @@ func (lm *LogMgr) Append(logrec []byte) (int, error) {
 			return 0, &Error{Op: "append", Err: fmt.Errorf("failed to flush: %v", err)}
 		}
 
-		if lm.currentBlock = lm.appendNewBlock(); lm.currentBlock == nil {
+		if lm.currentBlock, err = lm.appendNewBlock(); lm.currentBlock == nil {
 			return 0, &Error{Op: "append", Err: fmt.Errorf("failed to append new block")}
 		}
+		if err := lm.logBuffer.GetContents().SetInt(0, lm.fm.BlockSize()); err != nil {
+			return 0, &Error{Op: "Pin", Err: fmt.Errorf("failed to append initial block")}
+		}
 
-		boundary, _ = lm.logPage.GetInt(0)
+		if err := lm.logBuffer.Flush(); err != nil {
+			return 0, &Error{Op: "Pin", Err: fmt.Errorf("failed to append initial block")}
+		}
+
+		boundary, _ = logPage.GetInt(0)
 	}
 
 	recpos := boundary - bytesNeeded
-	if err := lm.logPage.SetBytes(recpos, logrec); err != nil {
+	if err := logPage.SetBytes(recpos, logrec); err != nil {
 		return 0, &Error{Op: "append", Err: fmt.Errorf("failed to set bytes: %v", err)}
 	}
 
-	if err := lm.logPage.SetInt(0, recpos); err != nil {
+	if err := logPage.SetInt(0, recpos); err != nil {
 		return 0, &Error{Op: "append", Err: fmt.Errorf("failed to update boundary: %v", err)}
 	}
 
 	lm.latestLSN++
+	lm.logBuffer.MarkModified(-1, lm.latestLSN)
 	return lm.latestLSN, nil
 }
 
--- utils/LogIterator.go
+++ utils/LogIterator.go
@@ -2,38 +2,46 @@ package utils
 
 import (
 	"fmt"
+	"ultraSQL/buffer"
 	"ultraSQL/kfile"
 	"unsafe"
 )
 
 type LogIterator struct {
 	fm         *kfile.FileMgr
+	bm         *buffer.BufferMgr
 	blk        *kfile.BlockId
-	p          *kfile.Page
+	buff       *buffer.Buffer
 	currentPos int
 	boundary   int
 }
 
-func NewLogIterator(fm *kfile.FileMgr, blk *kfile.BlockId) *LogIterator {
-	b := make([]byte, fm.BlockSize())
-	p := kfile.NewPageFromBytes(b)
-	iterator := &LogIterator{fm: fm, blk: blk, p: p}
-	iterator.moveToBlock(blk)
+func NewLogIterator(fm *kfile.FileMgr, bm *buffer.BufferMgr, blk *kfile.BlockId) *LogIterator {
+
+	iterator := &LogIterator{fm: fm, bm: bm, blk: blk}
+	err := iterator.moveToBlock(blk)
+	defer func() {
+		if err != nil && iterator != nil {
+			iterator.Close()
+		}
+	}()
 	return iterator
 }
 
 func (it *LogIterator) HasNext() bool {
-	return it.currentPos < it.fm.BlockSize() && it.blk.Number() > 0
+	return it.currentPos < it.fm.BlockSize() || it.blk.Number() > 0
 }
 
 func (it *LogIterator) Next() ([]byte, error) {
 	if it.currentPos == it.fm.BlockSize() {
 		it.blk = kfile.NewBlockId(it.blk.GetFileName(), it.blk.Number()-1)
-		it.moveToBlock(it.blk)
+		if err := it.moveToBlock(it.blk); err != nil {
+			return nil, err
+		}
 	}
-	rec, err := it.p.GetBytesWithLen(it.currentPos)
+	rec, err := it.buff.GetContents().GetBytesWithLen(it.currentPos)
 	if err != nil {
-		_ = fmt.Errorf("error while getting bytes %s", err)
+		return nil, fmt.Errorf("error while getting bytes: %v", err)
 	}
 	recLen := string(rec)
 	npos := MaxLength(len(recLen))
@@ -42,8 +50,29 @@ func (it *LogIterator) Next() ([]byte, error) {
 	return rec, nil
 }
 
-func (it *LogIterator) moveToBlock(blk *kfile.BlockId) {
-	it.fm.Read(blk, it.p)
-	it.boundary, _ = it.p.GetInt(0)
+func (it *LogIterator) moveToBlock(blk *kfile.BlockId) error {
+	if it.buff != nil {
+		err := it.buff.UnPin()
+		if err != nil {
+			return err
+		}
+	}
+	var err error
+	it.buff, err = it.bm.Pin(blk)
+	if err != nil {
+		return err
+	}
+	it.boundary, _ = it.buff.GetContents().GetInt(0)
 	it.currentPos = it.boundary
+	return nil
+}
+
+func (it *LogIterator) Close() {
+	if it.buff != nil {
+		err := it.buff.UnPin()
+		if err != nil {
+			panic(err)
+		}
+	}
+
 }
