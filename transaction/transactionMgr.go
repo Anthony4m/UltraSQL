3674365110ad3e40976453ec1211281dcfc5f6b5
anthony4m <amoahanthony41@gmail.com>
2025-02-05 18:38:22 +0000

    feat: Improve transaction manager (transaction)
    Refactor transaction manager to improve code style and
    add functionality.  Added error handling and improved
    concurrency control.  Added FindCell and InsertCell
    functions.

--- transaction/transactionMgr.go
+++ transaction/transactionMgr.go
@@ -20,10 +20,10 @@ type TransactionMgr struct {
 
 func NewTransaction(fm *kfile.FileMgr, lm *log.LogMgr, bm *buffer.BufferMgr) *TransactionMgr {
 	tx := &TransactionMgr{
-		fm:     fm,
-		bm:     bm,
-		txtnum: nextTxNumber(),
+		fm: fm,
+		bm: bm,
 	}
+	tx.nextTxNum = tx.nextTxNumber()
 	tx.rm = NewRecoveryMgr(tx, tx.txtnum, lm, bm)
 	tx.cm = NewConcurrencyMgr()
 	tx.bufferList = NewBufferList(bm)
@@ -54,28 +54,52 @@ func (t *TransactionMgr) UnPin(blk *kfile.BlockId) {
 	bufferList.UnPin(blk)
 }
 
-func (t *TransactionMgr) Size(filename string)int {
-	dummyblk := kfile.NewBlockId(filename, t.EndOfFile);
-	t.cm.sLock(dummyblk);
-	fileLength,err := t.fm.LengthLocked(filename);
-	if err != nil{
+func (t *TransactionMgr) Size(filename string) int {
+	dummyblk := kfile.NewBlockId(filename, t.EndOfFile)
+	t.cm.sLock(dummyblk)
+	fileLength, err := t.fm.LengthLocked(filename)
+	if err != nil {
 		return 0
 	}
-	return fileLength;
+	return fileLength
 }
 
 func (t *TransactionMgr) append(filename string) *kfile.BlockId {
-	BlockId dummyblk = new BlockId(filename, END_OF_FILE);
-	concurMgr.xLock(dummyblk);
-	return fm.append(filename);
+	dummyblk := kfile.NewBlockId(filename, t.EndOfFile)
+	t.cm.xLock(dummyblk)
+	blk, err := t.fm.Append(filename)
+	if err != nil {
+		return nil
+	}
+	return blk
 }
-func (t *TransactionMgr)  blockSize() int {
-	return t.fm.BlockSize();
+func (t *TransactionMgr) blockSize() int {
+	return t.fm.BlockSize()
 }
-func (t *TransactionMgr)  AvailableBuffs() int {
-	return t.bm.Available();
+func (t *TransactionMgr) AvailableBuffs() int {
+	return t.bm.Available()
 }
 
 func (t *TransactionMgr) nextTxNumber() int64 {
 	return atomic.AddInt64(&t.nextTxNum, 1)
+}
+
+func (t *TransactionMgr) FindCell(blk kfile.BlockId, key []byte) *kfile.Cell {
+	t.cm.sLock(blk)
+	buff := t.bufferList.getBuffer(blk)
+	return buff.Contents().FindCell(key)
+}
+
+func (t *TransactionMgr) InsertCell(blk kfile.BlockId, key []byte, val any, okToLog bool) {
+	t.cm.xLock(blk)
+	buff := t.bufferList.getBuffer(blk)
+	lsn := -1
+	if okToLog {
+		lsn = t.rm.setValue(buff, key, val)
+	}
+	cellKey := t.txtnum
+	cell := kfile.NewKVCell(cellKey)
+	p := buff.Contents()
+	p.InsertCell(cell)
+	buff.setModified(txnum, lsn)
 }
